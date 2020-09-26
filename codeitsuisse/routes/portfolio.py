0f95fbcf8f5cab4f4859b55f8a53965e79a7ab01
CHONG, Cheuk Hei <chchongaa@connect.ust.hk>
2020-09-27 07:15:36 +0800

    bug fix

--- codeitsuisse/routes/portfolio.py
+++ codeitsuisse/routes/portfolio.py
@@ -34,10 +34,10 @@ def main():
             ratio[index[j].get("Name")] = coefficient[index[j].get("Name")] * sd_s[data.get("Portfolio").get("Name")] / sd_f[index[j].get("Name")]
             numContracts[index[j].get("Name")] = ratio[index[j].get("Name")] * val[data.get("Portfolio").get("Name")] / (price[index[j].get("Name")]*notional[index[j].get("Name")])
 
-        ans = {"Name": index[0].get("Name"), "OptimalHedgeRatio": ratio[index[0].get("Name"), "Notional": numContracts[index[0].get("Name")]}
+        ans = {"Name": index[0].get("Name"), "OptimalHedgeRatio": ratio[index[0].get("Name")], "Notional": numContracts[index[0].get("Name")]}
         for j in range(len(index)):
             if (ratio[j] < ans_ratio):
-                ans = {"Name": index[j].get("Name"), "OptimalHedgeRatio": ratio[index[j].get("Name"), "Notional": numContracts[index[j].get("Name")]}
+                ans = {"Name": index[j].get("Name"), "OptimalHedgeRatio": ratio[index[j].get("Name")], "Notional": numContracts[index[j].get("Name")]}
         
     result.append(ans)
 
