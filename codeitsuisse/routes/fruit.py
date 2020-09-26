a7bc9d6045672f1caebc14843c14374d2611d4db
CHONG, Cheuk Hei <chchongaa@connect.ust.hk>
2020-09-26 22:18:08 +0800

    Update fruit.py

--- codeitsuisse/routes/fruit.py
+++ codeitsuisse/routes/fruit.py
@@ -17,7 +17,7 @@ def main():
     # banana = data.get("maBanana")
     total = 0
     for key, value in data.items():
-        total += value * 300
+        total += value * 30
 
 
     # guess = apple * 50 + watermelon * 50 + banana * 50
