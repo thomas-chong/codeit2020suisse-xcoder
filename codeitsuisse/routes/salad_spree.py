3687ff2a1376dae2f4724b68df86acf25cca2261
CHONG, Cheuk Hei <chchongaa@connect.ust.hk>
2020-09-26 10:29:36 +0800

    test

--- codeitsuisse/routes/salad_spree.py
+++ codeitsuisse/routes/salad_spree.py
@@ -7,41 +7,14 @@ from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
-@app.route('/salad-spree', methods=['POST'])
-def evaluateSecretMessage():
-    data = request.get_json()
+@app.route('/salad', methods=['POST'])
+def evaluateSquare():
+    data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    n = data.get("number_of_salads");
-    street_map = data.get("salad_prices_street_map");
-    #print()
-    result = street_map
+    inputValue = data.get("input");
+    result = inputValue * inputValue
+    logging.info("My result :{}".format(result))
+    return json.dumps(result);
 
-
-    logging.info("result :{}".format(result))
-    
-    return jsonify(result)
-
-# def search(n, street_map):
-#     res = []
-#     for i in range(len(street_map)):
-#         cnt = 0
-#         length = 0
-#         for j in range(len(street_map[0])):
-#             if street_map[i][j] == ‘X’:
-#                 cnt = 0
-#             else:
-#                 cnt += int(street_map[i][j])
-# 			    length += 1
-#                 if length >= n:
-#                     res.append(cnt)
-#     if not res:
-#         return 0
-#     return min(res)
-    
-
-    
-    
-    
-    
 
 
