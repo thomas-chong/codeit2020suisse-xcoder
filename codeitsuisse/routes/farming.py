869b6e39ce227d385aed9eecba30d15bd147a061
CHONG, Cheuk Hei <chchongaa@connect.ust.hk>
2020-09-26 22:57:00 +0800

    Update farming.py

--- codeitsuisse/routes/farming.py
+++ codeitsuisse/routes/farming.py
@@ -10,7 +10,9 @@ logger = logging.getLogger(__name__)
 
 @app.route('/intelligent-farming', methods=['POST'])
 def main():
-    data = request.get_json()
+    raw = request.get_data()
+    data = json.loads(raw.decode('utf-8'))
+    # data = request.get_json()
     #logging.info("data sent for evaluation {}".format(data))
     l = data.get("list")
     for i in range(len(l)):
