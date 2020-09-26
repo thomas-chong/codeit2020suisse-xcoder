2e6e0a8b0d8d9984d632de0191d8056eb48a5d2c
CHONG, Cheuk Hei <chchongaa@connect.ust.hk>
2020-09-27 06:15:24 +0800

    Update olympiad.py

--- codeitsuisse/routes/olympiad.py
+++ codeitsuisse/routes/olympiad.py
@@ -27,20 +27,27 @@ def optimalNum(numBook, numDay, books, days):
         targetTime = days[i]
         res = []
         ans = []
-        print(remain_books)
+        #print(remain_books)
         dfs(remain_books, targetTime, 0, res, [])
         for j in range(len(res)):
             if (sum(res[j]) > targetTime):
                 res[j].remove(res[j][-1])
-        #print(res)
+        print(res)
+        
+        max_length = len(res[0])
         min = targetTime - sum(res[0])
         target = res[0]
         for j in range(len(res)):
             diff = targetTime - sum(res[j])
-            if (diff < min and diff >= 0):
+            length = int(len(res[j]))
+            if (length > max_length):
+                target = res[j]
+                max_length = length
+            elif (diff < min and diff >= 0 and res[j] == max_length):
                 min = diff
                 target = res[j]
-        #print(target)
+        print(target)
+        
         count += len(target)
         for j in range(len(target)):
             remain_books.remove(target[j])
